31e045e7248155c665ee89d97d3aea50cb2428a2
Kevin-Cornelio <corneliolionel10@gmail.com>
2026-05-27 16:54:18 -0300

    Portafolio completo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,28 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+
+import { Navbar } from './components/navbar/navbar';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Skills } from './pages/skills/skills';
+import { Projects } from './pages/projects/projects';
+import { Contact } from './pages/contact/contact';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    Navbar,
+    Home,
+    About,
+    Skills,
+    Projects,
+    Contact,
+    Footer
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
-  protected readonly title = signal('portafolio');
+
 }
